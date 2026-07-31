b5bab36a73dfed1fac3dbf5a265695e01b116c42
Rivo87 <157936838+Rivo87@users.noreply.github.com>
2026-07-31 09:22:31 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,7 +73,7 @@ PrintMotd no
 AcceptEnv LANG LC_*
 Subsystem sftp /usr/lib/ssh/sftp-server
 Banner /etc/ssh/ssh_banner
-VersionAddendum بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+VersionAddendum [tytydkuda_speed]
 
 # 🛠 KUNCI UTAMA ANTI TIMEOUT:
 UseDNS no
